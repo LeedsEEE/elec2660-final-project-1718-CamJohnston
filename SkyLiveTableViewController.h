7ecc2f8b2581bfd69b577626b8b8fd7d9e5ca9a3
Cameron Johnston [el16cj] <el16cj@eentpc03.leeds.ac.uk>
2017-11-20 13:23:59 +0000

    Added Navigation Controller and Detail View

---
+++ SkyLiveTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SkyLiveTableViewController.h
+//  UK Night Sky Live
+//
+//  Created by Cameron Johnston [el16cj] on 20/11/2017.
+//  Copyright © 2017 Cameron Johnston [el16cj]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkyLiveTableViewController : UITableViewController
+
+@end
